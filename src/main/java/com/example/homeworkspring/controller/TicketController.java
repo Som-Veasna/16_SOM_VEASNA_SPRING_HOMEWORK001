97eb250925c6d1f9468cff371c7aff2e697d0b1a
Veasna <somveasna00@gmail.com>
2026-03-11 18:06:29 +0700

    update code version

--- src/main/java/com/example/homeworkspring/controller/TicketController.java
+++ src/main/java/com/example/homeworkspring/controller/TicketController.java
@@ -9,6 +9,7 @@ import com.example.homeworkspring.response.NotFoundResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -16,7 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/ticket")
+@RequestMapping("/api/v1/tickets")
 public class TicketController {
     List<Ticket> tickets=new ArrayList<>();
     public TicketController() {
@@ -28,7 +29,7 @@ public class TicketController {
     @GetMapping
     public ResponseEntity<?> getTickets() {
         ApiResponse<List<Ticket>> response=new ApiResponse<>(
-                true,"Succesfully",HttpStatus.OK,LocalDateTime.now(),tickets
+                true,"Succesfully",HttpStatus.OK,tickets,LocalDateTime.now()
         );
       return new ResponseEntity<>(response, HttpStatus.OK);
     }
@@ -37,7 +38,7 @@ public class TicketController {
     public ResponseEntity<?> addTicket(@RequestBody TicketRequest ticket) {
         Ticket ticket1=new Ticket(ticket.getPassengerName(),ticket.getTravelDate(),ticket.getSourceStation(),ticket.getDestinationStation(),ticket.getPrice(),ticket.getPaymentStatus(),ticket.getTicketStatus().toUpperCase(),ticket.getSeatNumber());
         tickets.add(ticket1);
-        ApiResponse<Ticket> response=new ApiResponse<>(true,"Succesfully",HttpStatus.OK,LocalDateTime.now(),ticket1);
+        ApiResponse<Ticket> response=new ApiResponse<>(true,"Succesfully",HttpStatus.OK,ticket1,LocalDateTime.now());
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
     @Operation(summary = "Get ticket by id")
@@ -45,7 +46,7 @@ public class TicketController {
     public ResponseEntity<?> getTicketById(@PathVariable("id") int id) {
         for (Ticket ticket : tickets) {
             if (ticket.getTicketId()==id) {
-                ApiResponse<Ticket> response=new ApiResponse<>(true,"Succesfully",HttpStatus.OK,LocalDateTime.now(),ticket);
+                ApiResponse<Ticket> response=new ApiResponse<>(true,"Succesfully",HttpStatus.OK,ticket,LocalDateTime.now());
                 return new ResponseEntity<>(response, HttpStatus.OK);
             }
         }
@@ -55,25 +56,32 @@ public class TicketController {
     @Operation(summary = "Get ticket by name")
     @GetMapping("/search")
     public ResponseEntity<?> searchByName(@RequestParam String name) {
+        String message;
+        List<Ticket> isTicketFound=new ArrayList<>();
           for (Ticket ticket : tickets) {
               if (ticket.getPassengerName().equals(name)) {
-                  ApiResponse<Ticket> response=new ApiResponse<>(true,"Succesfully",HttpStatus.OK,LocalDateTime.now(),ticket);
-                  return new ResponseEntity<>(response, HttpStatus.OK);
+                  isTicketFound.add(ticket);
               }
           }
-        NotFoundResponse notFoundResponse=new NotFoundResponse(false,"No Ticket found with Id:"+name,HttpStatus.NOT_FOUND, LocalDateTime.now());
-          return new ResponseEntity<>(notFoundResponse, HttpStatus.NOT_FOUND);
+          if(isTicketFound.isEmpty()){
+              message="No Ticket found with name:"+name;
+          }else {
+              message="Ticket found with name:"+name;
+          }
+        ApiResponse<List<Ticket>> response=new ApiResponse<>(true,message,HttpStatus.OK,isTicketFound,LocalDateTime.now());
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
     @Operation(summary = "Get ticket by status and Travel date")
     @GetMapping("/filter")
-    public ResponseEntity<?> FiterTicketByStatusAndTravelDate(@RequestParam TicketStatus ticketStatus, @RequestParam String travelDate) {
+    public ResponseEntity<?> fiterTicketByStatusAndTravelDate(@RequestParam TicketStatus ticketStatus, @RequestParam String travelDate) {
         for (Ticket ticket : tickets) {
-            if (ticket.getTicketStatus().equals(ticketStatus.toString())&&ticket.getTravelDate().equalsIgnoreCase(ticket.getTravelDate())) {
-                ApiResponse<Ticket> response=new ApiResponse<>(true,"Succesfully",HttpStatus.OK,LocalDateTime.now(),ticket);
+            if (ticket.getTicketStatus().equals(ticketStatus.toString())&&ticket.getTravelDate().equals(travelDate)) {
+                ApiResponse<Ticket> response=new ApiResponse<>(true,"Succesfully",HttpStatus.OK,ticket,LocalDateTime.now());
                 return new ResponseEntity<>(response, HttpStatus.OK);
             }
         }
-       return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+        NotFoundResponse notFoundResponse=new NotFoundResponse(false,"Ticket status and Travel date not found",HttpStatus.NOT_FOUND, LocalDateTime.now());
+       return new ResponseEntity<>(notFoundResponse, HttpStatus.NOT_FOUND);
     }
     @Operation(summary = "Add multiple ticket")
     @PostMapping("/bulk")
@@ -84,7 +92,7 @@ public class TicketController {
             Ticket multipleTicket= new Ticket(ticket1.getPassengerName(), ticket1.getTravelDate(), ticket1.getSourceStation(), ticket1.getDestinationStation(), ticket1.getPrice(), ticket1.getPaymentStatus(), ticket1.getTicketStatus(), ticket1.getSeatNumber());
             tickets.add(multipleTicket);
             addTickets.add(multipleTicket);
-            ApiResponse<List<Ticket>> response = new ApiResponse<>(true, "Succesfully", HttpStatus.OK, LocalDateTime.now(), newTickets);
+            ApiResponse<List<Ticket>> response = new ApiResponse<>(true, "Succesfully", HttpStatus.OK,newTickets, LocalDateTime.now());
             return new ResponseEntity<>(response, HttpStatus.OK);
         }
         return ResponseEntity.badRequest().build();
@@ -95,8 +103,7 @@ public class TicketController {
         for (Ticket ticket : tickets) {
             if (ticket.getTicketId()==id) {
                 tickets.remove(ticket);
-                ApiResponse<Ticket> response=new ApiResponse<>(true,"Succesfully",HttpStatus.OK,LocalDateTime.now(),ticket);
-
+                ApiResponse<Ticket> response=new ApiResponse<>(true,"Succesfully",HttpStatus.OK,null,LocalDateTime.now());
                 return new ResponseEntity<>(response, HttpStatus.OK);
             }
         }
@@ -116,7 +123,7 @@ public class TicketController {
                 ticket1.setPaymentStatus(ticket.getPaymentStatus());
                 ticket1.setTicketStatus(ticket.getTicketStatus());
                 ticket1.setSeatNumber(ticket.getSeatNumber());
-                ApiResponse<Ticket> response =new ApiResponse<>(true,"Succesfully",HttpStatus.OK,LocalDateTime.now(),ticket1);
+                ApiResponse<Ticket> response =new ApiResponse<>(true,"Succesfully",HttpStatus.OK,ticket1,LocalDateTime.now());
                 return new ResponseEntity<>(response, HttpStatus.OK);
             }
         }
@@ -135,7 +142,7 @@ public class TicketController {
                 }
             }
         }
-        ApiResponse<List<Ticket>> response=new ApiResponse<>(true,"Succesfully",HttpStatus.OK,LocalDateTime.now(),ticketUpdate);
+        ApiResponse<List<Ticket>> response=new ApiResponse<>(true,"Succesfully",HttpStatus.OK,ticketUpdate,LocalDateTime.now());
        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
--- src/main/java/com/example/homeworkspring/response/ApiResponse.java
+++ src/main/java/com/example/homeworkspring/response/ApiResponse.java
@@ -1,17 +1,18 @@
 package com.example.homeworkspring.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
 @AllArgsConstructor
 public class ApiResponse <T>{
     Boolean success;
     private String message;
     HttpStatus status;
-    private LocalDateTime timestamp;
     private T Payload;
-
+    private LocalDateTime timestamp;
 }
